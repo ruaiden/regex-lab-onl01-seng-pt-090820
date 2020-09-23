b9ee4973edad61ef09bc4bb682d3c81d83cb56bc
Aiden Marques <aiden.marques94@gmail.com>
2020-09-23 20:14:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/(\D\d{3}\D|\d{3})(\d{3}|\D\d{3})(\d{4}|\D\d{4})\z/])
+  if phone.match(/(\D\d{3}\D|\d{3})(\d{3}|\D\d{3})(\d{4}|\D\d{4})\z/)
 endd{4}|\D\d{4})\z)
     return true 
   else 
