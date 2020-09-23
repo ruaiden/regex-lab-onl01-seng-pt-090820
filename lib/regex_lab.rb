9853299bc84e6bbb31e12a9891c4dd8f121e894d
Aiden Marques <aiden.marques94@gmail.com>
2020-09-23 19:48:59 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,39 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/^[AaEeIiOoUu]/)
+    return true 
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.match(/un\w+ing/i)
+    return true 
+  else 
+    return false 
+  end
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(words_string)
+  if words_string.match /^\w{5}$/
+    return true
+  else
+    return false
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.match(/^[A-Z]/) && text.match(/$[.,?!]/)
+    return true 
+  else
+    return false
+  end 
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match
+    return true 
+  else 
+    return false
+  end
 end
